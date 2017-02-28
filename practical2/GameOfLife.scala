d7ff8cd71f368ad6bfcf123cdaa5417172876313
Michael Weisz <mail@michael-weisz.de>
2017-02-28 02:18:35 +0000

    Implement optional variations (Seeds, Highlife)

--- practical2/GameOfLife.scala
+++ practical2/GameOfLife.scala
@@ -30,7 +30,31 @@ object GameOfLife {
             board(x)(y) = true
         }
 
+        // addReplicator(board)
+
+
         board
+    }
+
+    def addReplicator(board: Array[Array[Boolean]] ) = {
+        // Replicator
+        var offset = 10
+        board(offset)(offset+2) = true
+        board(offset)(offset+3) = true
+        board(offset)(offset+4) = true
+        
+        board(offset+1)(offset+1) = true
+        board(offset+1)(offset+4) = true
+
+        board(offset+2)(offset) = true
+        board(offset+2)(offset+4) = true
+
+        board(offset+3)(offset) = true
+        board(offset+3)(offset+3) = true
+
+        board(offset+4)(offset) = true
+        board(offset+4)(offset+1) = true
+        board(offset+4)(offset+2) = true
     }
 
     def createWorker(name: String, id: Int) = proc {
@@ -46,7 +70,6 @@ object GameOfLife {
 
         while(!finished) {
             barrier.sync // Make sure they all have written their rows
-            // Thread.sleep(50)
 
             val updatedRows = computeUpdatedRows(startRow, numRows)
 
@@ -86,7 +109,14 @@ object GameOfLife {
         // Compute new state of rows
         for (rowId <- 0 until rowCount) {
             for (colId <- 0 until N) {
+                // Game of Life
                 updatedRows(rowId)(colId) = computeNewState(rowId + startRow, colId)
+
+                // Seeds
+                // updatedRows(rowId)(colId) = computeNewStateSeeds(rowId + startRow, colId)
+
+                // Highlife
+                // updatedRows(rowId)(colId) = computeNewStateHighlife(rowId + startRow, colId)
             }
         }
 
@@ -94,7 +124,7 @@ object GameOfLife {
     }
 
     def computeNewState(r: Int, c: Int) : Boolean = {
-        val livingNeighbours = countLivingNeighbours(r,c)
+        val livingNeighbours = countLivingNeighboursWrap(r,c)
 
         var newState = board(r)(c)
 
@@ -103,6 +133,39 @@ object GameOfLife {
             newState = false
         } else if (livingNeighbours == 3) {
             newState = true
+        }
+
+        return newState
+    }
+
+    // OPTIONAL: 
+    def computeNewStateSeeds(r: Int, c: Int) : Boolean = {
+        val livingNeighbours = countLivingNeighboursWrap(r,c)
+
+        var newState = board(r)(c)
+
+        // Seeds
+        if(!newState && livingNeighbours == 2) {
+            newState = true
+        } else {
+            newState = false
+        }
+
+        return newState
+    }
+
+    def computeNewStateHighlife(r: Int, c: Int) : Boolean = {
+        val livingNeighbours = countLivingNeighboursWrap(r,c)
+
+        var newState = board(r)(c)
+
+        var born = !newState && (livingNeighbours == 3 || livingNeighbours == 6)
+        var survive = newState && (livingNeighbours == 2 || livingNeighbours == 3)
+
+        if(born || survive) {
+            newState = true
+        } else {
+            newState = false
         }
 
         return newState
@@ -126,6 +189,22 @@ object GameOfLife {
             if (c > 0 && board(r+1)(c-1)) { livingNeighbours += 1}
             if (board(r+1)(c)) { livingNeighbours += 1}
             if (c < N - 1 && board(r+1)(c+1)) { livingNeighbours += 1}
+        }
+
+        livingNeighbours
+    }
+
+    def countLivingNeighboursWrap(r: Int, c: Int) : Int = {
+        var livingNeighbours = 0
+
+        for (x <- r-1 to r+1) {
+            for (y <- c-1 to c+1) {
+                if (!(x == r && y == c)) {
+                    if (board((x+N)%N)((y+N)%N)) {
+                        livingNeighbours += 1
+                    }
+                }
+            }
         }
 
         livingNeighbours
